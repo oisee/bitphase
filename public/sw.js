71a4c6f133d7cbfc6a72507eb67cdcebb7743814
Kamil Patecki <paatorek@gmail.com>
2026-02-08 21:28:55 +0100

    Use network-first strategy for caching

--- public/sw.js
+++ public/sw.js
@@ -9,22 +9,31 @@ self.addEventListener('install', (event) => {
 	);
 });
 
+self.addEventListener('activate', (event) => {
+	event.waitUntil(
+		caches
+			.keys()
+			.then((keys) =>
+				Promise.all(
+					keys.filter((key) => key !== CACHE_BUCKET).map((key) => caches.delete(key))
+				)
+			)
+			.then(() => self.clients.claim())
+	);
+});
+
 self.addEventListener('fetch', (event) => {
 	event.respondWith(
-		caches.match(event.request).then((cachedResponse) => {
-			if (cachedResponse) return cachedResponse;
-
-			return fetch(event.request).then((networkResponse) => {
+		fetch(event.request)
+			.then((networkResponse) => {
 				if (networkResponse && networkResponse.ok) {
 					const responseToCache = networkResponse.clone();
-
 					caches.open(CACHE_BUCKET).then((cache) => {
 						cache.put(event.request, responseToCache);
 					});
 				}
-
 				return networkResponse;
-			});
-		})
+			})
+			.catch(() => caches.match(event.request))
 	);
 });
